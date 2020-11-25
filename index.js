fdc925851875fc5a92bf687f7cf3c3c06bf33442
Eason Lin <uu9924079@gmail.com>
2020-11-25 21:25:24 +0800

    初始化...專案...

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
