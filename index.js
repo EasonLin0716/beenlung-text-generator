d472f154d45143fe948ade0285e2d35eb7215340
Eason Lin <uu9924079@gmail.com>
2020-11-26 22:21:32 +0800

    安裝...pug...

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('index')
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}.`)
