92b07b656a2e1e3433c07e12b426d4e25bbad3de
Eason Lin <uu9924079@gmail.com>
2020-11-28 16:26:18 +0800

    安裝...jieba...

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const info = require('./public/json/info.json')
+const nodejieba = require("nodejieba");
+nodejieba['DEFAULT_DICT'] = './dict/jieba.dict.utf8'
+nodejieba['DEFAULT_HMM_DICT'] =  './dict/hmm_model.utf8'
+nodejieba['DEFAULT_USER_DICT'] =  './dict/user.dict.utf8'
+nodejieba['DEFAULT_IDF_DICT'] = './dict/idf.utf8'
+nodejieba['DEFAULT_STOP_WORD_DICT'] =  './dict/stop_words.utf8'
+const punctuationMarksRegex = require('./public/js/punctuationMarks')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
